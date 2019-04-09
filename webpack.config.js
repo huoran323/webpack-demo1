1f5a0e7794b1166b927e32b3eedb6cabfdcc2a06
huoran <git@huoran.com>
2019-04-09 22:21:28 +0800

    entry output

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
 const path = require("path"); //引入一个名字叫path的node的核心模块
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 // plugin 可以再webpack运行到某个时刻的时候， 帮你做一些事情
 
 module.exports = {
   mode: "development", //模式 development:bundle.js不会被压缩  production: bundle.js会被压缩
-  entry: "./src/index.js", //从index.js开始打包
-  module: { 
+  // entry: "./src/index.js", //从index.js开始打包
+  entry: {
+    main: "./src/index.js",
+    sub: "./src/index.js" //打包两个文件
+  },
+  module: {
     //模块配置
     rules: [
-
       //url-loader  使用url-loader打包图片，会将图片转换成一个base64的字符串，然后直接放到bundle.js里面，而不是单独生成一个图片，适用于图片较小的情况
       {
-      test: /\.(jpg|png|gif)$/, //jpg格式的文件  (jpg|png|gif)可以打包多种格式的图片文件
+        test: /\.(jpg|png|gif)$/, //jpg格式的文件  (jpg|png|gif)可以打包多种格式的图片文件
         use: {
           loader: "url-loader", //用url-loader来打包jpg格式的文件  需要安装url-loader  npm install url-loader --save-dev
-          options: { //配置项
+          options: {
+            //配置项
             //placeholder 占位符
-            name: '[name].[ext]', //打包文件的名字及后缀与源文件一样 此处以jpg文件格式为例 
-            outputPath: 'images/', //将图片格式的文件打包到dist下的images文件夹里
-            limit: 2048,  //如果图片的大小超过2048，就像file-loader一样，将打包的图片放到dist下 
+            name: "[name].[ext]", //打包文件的名字及后缀与源文件一样 此处以jpg文件格式为例
+            outputPath: "images/", //将图片格式的文件打包到dist下的images文件夹里
+            limit: 2048 //如果图片的大小超过2048，就像file-loader一样，将打包的图片放到dist下
           }
         }
       },
-      //file-loader  使用file-loader打包图片，会在dist文件下生成对应的图片 
+      //file-loader  使用file-loader打包图片，会在dist文件下生成对应的图片
       /* {
         test: /\.(jpg|png|gif)$/, //jpg格式的文件  (jpg|png|gif)可以打包多种格式的图片文件
         use: {
@@ -36,37 +40,42 @@ module.exports = {
           }
         }
       } */
-      { 
+      {
         test: /\.css$/, //打包css文件
-        use: ['style-loader',  //loader的执行顺序：从下到上，从右到左 
-              // 'css-loader',
-              {  //给css-loader添加配置项
-                loader: 'css-loader',
-                options: {
-                  // importLoaders: 1, //对于在css/scss/less文件中引用css/scss/less文件，importLoaders: 2 规定内部的css/scss/less文件也要重新走一遍下面两个loader 
-                  // modules: false, //开启css的模块化打包，
-                }
-              },
-              // 'sass-loader',  //打包sass文件
-              // 'postcss-loader'  //厂商前缀
+        use: [
+          "style-loader", //loader的执行顺序：从下到上，从右到左
+          // 'css-loader',
+          {
+            //给css-loader添加配置项
+            loader: "css-loader",
+            options: {
+              // importLoaders: 1, //对于在css/scss/less文件中引用css/scss/less文件，importLoaders: 2 规定内部的css/scss/less文件也要重新走一遍下面两个loader
+              // modules: false, //开启css的模块化打包，
+            }
+          }
+          // 'sass-loader',  //打包sass文件
+          // 'postcss-loader'  //厂商前缀
         ]
       },
       {
         test: /\.(eot|ttf|svg|woff)$/, //打包字体
         use: [
-          "file-loader" // 
+          "file-loader" //
         ]
-      } 
+      }
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({   //html-webpack-plugin插件会在打包结束后，自动生成一个html文件，并把打包生成的js自动引入到这个html文件中
-      template: 'src/index.html',  //模板文件，
+    new HtmlWebpackPlugin({
+      //html-webpack-plugin插件会在打包结束后，自动生成一个html文件，并把打包生成的js自动引入到这个html文件中
+      template: "src/index.html" //模板文件，
     }),
-    new CleanWebpackPlugin(), // 清除插件
-  ], 
+    new CleanWebpackPlugin() // 清除插件
+  ],
   output: {
-    filename: "bundle.js",
+    publicPath: "http://cdn.com.cn", //打包上线的外网地址
+    filename: "[name].js", //对应入口的两个打包的文件名main sub
+    // filename: "bundle.js",
     path: path.resolve(__dirname, "dist") //__dirname指的是当前文件所在的路径  生成bundle文件夹的绝对路径
   }
 };
